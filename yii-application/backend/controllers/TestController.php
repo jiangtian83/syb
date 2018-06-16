d91d18bcef5ea05fdbd089068ca8803bf84e3766
jiangtian <284053253@qq.com>
2018-06-16 13:16:52 +0800

    ts

---
+++ yii-application/backend/controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: jt
+ * Date: 2018/6/16
+ * Time: 下午1:16
+ * E-Mail: 284053253@qq.com
+ * QQ: 284053253
+ */
+
+namespace backend\controllers;
+
+
+class TestController
+{
+    public function actionIndex(){
+        return 8888;
+    }
+}
