5f6339562229343141537da1794367e7f7e98c4a
jiangtian <284053253@qq.com>
2018-06-16 18:22:15 +0800

    ts

--- yii-application/backend/views/layouts/main.php
+++ yii-application/backend/views/layouts/main.php
@@ -70,8 +70,6 @@ AppAsset::register($this);
 <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= JTYii::powered() ?></p>
     </div>
 </footer>
 
