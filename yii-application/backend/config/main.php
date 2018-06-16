62eff05b82927f609161c4753e2bb4ab186de9b1
jiangtian <284053253@qq.com>
2018-06-16 17:01:25 +0800

    ts

--- yii-application/backend/config/main.php
+++ yii-application/backend/config/main.php
@@ -21,7 +21,7 @@ return [
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'loginUrl' => 'admin/login',
+            'loginUrl' => '/admin/login',
             'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
         ],
         'session' => [
