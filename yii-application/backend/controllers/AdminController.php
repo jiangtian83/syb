c0a371f3344ae6519a51ab01e68b99eb2673ceab
jiangtian <284053253@qq.com>
2018-06-16 13:59:43 +0800

    ts

--- yii-application/backend/controllers/AdminController.php
+++ yii-application/backend/controllers/AdminController.php
@@ -64,7 +64,7 @@ class AdminController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->render('test');
     }
 
 
