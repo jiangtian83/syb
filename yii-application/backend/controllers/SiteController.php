518e6797ec3855731593db503cf082da7a65e253
jiangtian <284053253@qq.com>
2018-06-16 13:21:12 +0800

    ts

--- yii-application/backend/controllers/SiteController.php
+++ yii-application/backend/controllers/SiteController.php
@@ -70,8 +70,6 @@ class SiteController extends Controller
      */
     public function actionLogin()
     {
-        return 88888;
-
         if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
