ecefff6404ec6319c46c9cbdf70c7bc87e495f87
jiangtian <284053253@qq.com>
2018-06-16 14:13:12 +0800

    ts

---
+++ yii-application/backend/views/admin/test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: jt
+ * Date: 2018/6/16
+ * Time: 下午1:58
+ * E-Mail: 284053253@qq.com
+ * QQ: 284053253
+ */
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>后台管理系统</title>
+</head>
+<body>
+<?= 1342143214231414 ?>
+</body>
+</html>
