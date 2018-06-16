595ee97c0ea88a347231c331ba71d3f44c895295
jiangtian <284053253@qq.com>
2018-06-16 19:12:15 +0800

    ts

--- yii-application/backend/views/admin/login.php
+++ yii-application/backend/views/admin/login.php
@@ -35,7 +35,7 @@ use yii\captcha\Captcha;
             <?=$form->field($model,'user')->textInput(["placeholder"=>"账号"]); ?>
             <?=$form->field($model,'pwd')->passwordInput(['placeholder'=>'密码']); ?>
             <?=$form->field($model,'verifyCode')->widget(Captcha::className(),['captchaAction'=>Yii::$app->urlManager->createUrl('admin/captcha'),
-                'imageOptions' => ['style' => 'height: 34px;'],
+                'imageOptions' => ['style' => 'cursor:pointer','alt'=>'点击换图','title'=>'点击换图'],
                 'template'=>'<div class="row"><div class="col-md-8 col-xs-6">{input}</div><div class="col-md-4 col-xs-6">{image}</div></div>'
             ])?>
             <?=  Html::submitButton('登录', ['class'=>'btn btn-primary btn-lg btn-block','name' =>'submit-button']) ?>
