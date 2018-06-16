0a16c282ea5a3fb80bdc51d7fd2675c2bad57b48
jiangtian <284053253@qq.com>
2018-06-16 18:01:06 +0800

    ts

--- yii-application/backend/controllers/AdminController.php
+++ yii-application/backend/controllers/AdminController.php
@@ -161,18 +161,16 @@ class AdminController extends Controller
         $model = new UserForm();
 
         if($model->load($data = Yii::$app->request->post())){
-            Yii::$app->response->format = Response::FORMAT_JSON;
-            return $data;
-//            if($model->login()){
-//                //查询未读消息
-////                $count=Msg::find()->andwhere(['tid'=>Yii::$app->user->getId(),'status'=>0])->count();
-////                $session=Yii::$app->session;
-////                $session->set('msg',$count);
-//
-//                return $this->redirect(['admin/index']);
-//            }else{
-//                return $this->render('login',['model'=>$model]);
-//            }
+            if($model->login()){
+                //查询未读消息
+//                $count=Msg::find()->andwhere(['tid'=>Yii::$app->user->getId(),'status'=>0])->count();
+//                $session=Yii::$app->session;
+//                $session->set('msg',$count);
+
+                return $this->redirect(['admin/index']);
+            }else{
+                return $this->render('login',['model'=>$model]);
+            }
         }
 
         return $this->render('login',['model'=>$model]);
