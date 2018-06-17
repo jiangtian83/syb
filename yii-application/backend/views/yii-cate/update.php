f0cb753b085c9d9a6f42e6e65ed518a65b5b2a6f
jiangtian <284053253@qq.com>
2018-06-18 00:52:36 +0800

    ts

--- yii-application/backend/views/yii-cate/update.php
+++ yii-application/backend/views/yii-cate/update.php
@@ -12,8 +12,6 @@ $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => "##"];
 ?>
 <div class="yii-cate-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
