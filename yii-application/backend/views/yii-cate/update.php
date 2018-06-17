55b7cce0545e25db57f8a09c9733b16675cef07c
jiangtian <284053253@qq.com>
2018-06-18 00:53:36 +0800

    ts

--- yii-application/backend/views/yii-cate/update.php
+++ yii-application/backend/views/yii-cate/update.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
 $this->title = Yii::t('app', '编辑：' . $model->id, [
     'nameAttribute' => '' . $model->id,
 ]);
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => "##"];
+$this->params['breadcrumbs'][] = ['label' => $model->catename, 'url' => "##"];
 ?>
 <div class="yii-cate-update">
 
