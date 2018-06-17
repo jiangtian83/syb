e6ef836f138f38e24c7b0bb0e24ec60c5d3c6dc3
jiangtian <284053253@qq.com>
2018-06-17 22:52:25 +0800

    ts

--- yii-application/backend/views/layouts/main-backend.php
+++ yii-application/backend/views/layouts/main-backend.php
@@ -29,6 +29,11 @@ AppAsset::register($this);
 
 <div class="wrap">
     <div class="container">
+        <?= Breadcrumbs::widget([
+            'homeLink' => ['/yii-cate/index'],
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
         <?= $content ?>
     </div>
 </div>
