dbffdba9a79ad208c19e4a5399bc5c2d6aa4cc9d
jiangtian <284053253@qq.com>
2018-06-18 16:59:16 +0800

    ts

--- yii-application/backend/views/yii-cate/__form.php
+++ yii-application/backend/views/yii-cate/__form.php
@@ -51,7 +51,7 @@ foreach ($cates as $cate){
             ]
     ]) ?>
 
-    <?= $form->field($model, 'type')->label(false)->hiddenInput(['value' => 2]) ?>
+    <?= $form->field($model, 'type')->label('单页类型')->radioList(['2' => '普通单页', '1' => '公司项目'], ['value' => 2]) ?>
 
     <?= $form->field($model, 'views', [
         'options' => ['style' => 'margin: 0']
