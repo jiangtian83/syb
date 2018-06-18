3d2928f10323ea47e0e6c5addf8f0605a74a0fe3
jiangtian <284053253@qq.com>
2018-06-18 14:09:35 +0800

    ts

--- yii-application/backend/controllers/YiiArtController.php
+++ yii-application/backend/controllers/YiiArtController.php
@@ -69,7 +69,7 @@ class YiiArtController extends Controller
         $model = new YiiArt();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['index']);
         }
 
         return $this->render('create', [
@@ -89,7 +89,7 @@ class YiiArtController extends Controller
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['index']);
         }
 
         return $this->render('update', [
