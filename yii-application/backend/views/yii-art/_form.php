57a3f46c65f5b834bdbef217db1223abe026cf5e
jiangtian <284053253@qq.com>
2018-06-18 15:40:35 +0800

    ts

--- yii-application/backend/views/yii-art/_form.php
+++ yii-application/backend/views/yii-art/_form.php
@@ -41,10 +41,6 @@ foreach ($cates as $cate){
         'options' => ['style' => 'margin: 0;']
     ])->textarea(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'content', [
-        'options' => ['style' => 'margin: 0;']
-    ])->textarea(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'content')->widget(CKEditor::className()) ?>
 
     <?= $form->field($model, 'cateid', [
