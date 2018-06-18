beaa329f15fdf3fd95415800cba52b6e9991b885
jiangtian <284053253@qq.com>
2018-06-18 17:16:34 +0800

    ts

--- yii-application/backend/views/yii-cate/__form.php
+++ yii-application/backend/views/yii-cate/__form.php
@@ -51,7 +51,7 @@ foreach ($cates as $cate){
             ]
     ]) ?>
 
-    <?= $form->field($model, 'type')->label('单页类型')->radioList(['2' => '普通单页', '1' => '公司项目'], ['value' => 2, 'labelOptions' => [ 'style' => 'display:inline']]) ?>
+    <?= $form->field($model, 'type', ['labelOptions' => [ 'style' => 'display:inline']])->label('单页类型')->radioList(['2' => '普通单页', '1' => '公司项目'], ['value' => 2]) ?>
 
     <?= $form->field($model, 'views', [
         'options' => ['style' => 'margin: 0']
