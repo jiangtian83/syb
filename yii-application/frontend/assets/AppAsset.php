25ba6eeaa5931872c87a0dca0d0e85e0f8a733e9
jiangtian <284053253@qq.com>
2018-06-18 19:00:04 +0800

    ts

--- yii-application/frontend/assets/AppAsset.php
+++ yii-application/frontend/assets/AppAsset.php
@@ -12,9 +12,17 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/fiexslider.css',
+        'css/smoothbox.css',
+        'css/style.css'
     ];
     public $js = [
+        'js/bootstrap.js',
+        'js/counterup.min.js',
+        'jquery.flexisel.js',
+        'jquery.flexslider.js',
+        'smoothbox.jquery2.js',
+        'waypoints.min.s'
     ];
     public $depends = [
         'yii\web\YiiAsset',
---
+++ yii-application/frontend/views/layouts/layout.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: jt
+ * Date: 2018/6/18
+ * Time: 下午6:50
+ * E-Mail: 284053253@qq.com
+ * QQ: 284053253
+ */
+use frontend\assets\AppAsset;
+
+AppAsset::register($this);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href='https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700,900' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<!-- start-smoth-scrolling -->
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
+        $(".scroll").click(function(event){
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+    });
+</script>
+<!-- start-smoth-scrolling -->
+</head>
+
+<body>
+<!-- header -->
+	<div class="header" id="ban">
+		<div class="container">
+			<div class="w3l_social_icons">
+				<ul>
+					<li><a href="#" class="facebook"></a></li>
+					<li><a href="#" class="twitter"></a></li>
+					<li><a href="#" class="google_plus"></a></li>
+					<li><a href="#" class="pinterest"></a></li>
+					<li><a href="#" class="instagram"></a></li>
+				</ul>
+			</div>
+			<div class="w3ls_logo">
+				<h1><a href="index.html">Barter</a></h1>
+			</div>
+			<div class="header_right">
+			<nav class="navbar navbar-default">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+					<nav class="link-effect-7" id="link-effect-7">
+						<ul class="nav navbar-nav">
+							<li class="active act"><a href="index.html" data-hover="Home">Home</a></li>
+							<li><a href="about.html" data-hover="About">About</a></li>
+							<li><a href="portfolio.html" data-hover="Portfolio">Portfolio</a></li>
+							<li><a href="short-codes.html" data-hover="Short Codes">Short Codes</a></li>
+							<li><a href="mail.html" data-hover="Mail">Mail</a></li>
+						</ul>
+					</nav>
+				</div>
+				<!-- /.navbar-collapse -->
+			</nav>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+<!-- //header -->
+
+
+<!-- footer -->
+<div class="footer">
+    <div class="container">
+        <h2>Subscribe to <span>Newsletter</span></h2>
+        <form action="#" method="post">
+            <input type="email" name="Email" value="Enter Your Email..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter Your Email...';}" required="">
+            <input type="submit" value="Send">
+        </form>
+    </div>
+</div>
+<div class="footer-copy">
+    <div class="container">
+        <div class="w3agile_footer_grids">
+            <div class="col-md-4 w3agile_footer_grid">
+                <h3>About Us</h3>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse.<span>Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit.</span></p>
+            </div>
+            <div class="col-md-4 w3agile_footer_grid">
+                <h3>Contact Info</h3>
+                <ul>
+                    <li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i>1234k Avenue, 4th block, <span>New York City.</span></li>
+                    <li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><a href="mailto:info@example.com">info@example.com</a></li>
+                    <li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>+1234 567 567</li>
+                </ul>
+            </div>
+            <div class="col-md-4 w3agile_footer_grid w3agile_footer_grid1">
+                <h3>Navigation</h3>
+                <ul>
+                    <li><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span><a href="about.html">About Us</a></li>
+                    <li><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span><a href="portfolio.html">Portfolio</a></li>
+                    <li><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span><a href="short-codes.html">Short Codes</a></li>
+                    <li><span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span><a href="mail.html">Mail Us</a></li>
+                </ul>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+    </div>
+</div>
+<div class="copy-right-social">
+    <div class="container">
+        <div class="footer-pos">
+            <a href="#ban" class="scroll"><img src="images/arrow.png" alt=" " class="img-responsive" /></a>
+        </div>
+        <div class="copy-right">
+            <p>Copyright &copy; 2016.Company name All rights reserved.More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a></p>
+        </div>
+        <div class="copy-right-social1">
+            <div class="w3l_social_icons w3l_social_icons1">
+                <ul>
+                    <li><a href="#" class="facebook"></a></li>
+                    <li><a href="#" class="twitter"></a></li>
+                    <li><a href="#" class="google_plus"></a></li>
+                    <li><a href="#" class="pinterest"></a></li>
+                    <li><a href="#" class="instagram"></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<!-- //footer -->
+<!-- for bootstrap working -->
+<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+</body>
+</html>
