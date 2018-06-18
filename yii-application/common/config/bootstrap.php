05d236932a85bc62f013a401e3df230ebe17bb0a
jiangtian <284053253@qq.com>
2018-06-18 08:11:27 +0800

    ts

--- yii-application/common/config/bootstrap.php
+++ yii-application/common/config/bootstrap.php
@@ -4,3 +4,4 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@yii/gii', dirname(dirname(__DIR__)) . '/vendor/yiisoft/yii2-gii/src');
+Yii::setAlias('@yii/debug', dirname(dirname(__DIR__)) . '/vendor/yiisoft/yii2-debug');
